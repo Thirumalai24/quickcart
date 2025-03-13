544ee7a714759f72a90235e39b3f075282d6ad58
Thirumalai24 <162083328+Thirumalai24@users.noreply.github.com>
2025-03-14 01:38:46 +0530

    slider issue fixed

--- quickcart/settings.py
+++ quickcart/settings.py
@@ -122,14 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-MEDIA_URL ='/images/'
-MEDIA_ROOT =BASE_DIR/'static'
-
-STATICFILES_DIR =[
-    BASE_DIR/'static'
+ 
+MEDIA_URL='/images/'
+MEDIA_ROOT=BASE_DIR/'static'
+ 
+STATICFILES_DIRS=[
+	BASE_DIR/'static'
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
