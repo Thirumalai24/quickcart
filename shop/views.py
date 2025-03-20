2b63486ad023eec5cfe99e3c5e8e27fe3627aaf2
Thirumalai24 <162083328+Thirumalai24@users.noreply.github.com>
2025-03-20 17:10:46 +0530

    footer page

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from . models import *
 from django.contrib import messages
 
@@ -15,8 +15,20 @@ def collections(request):
 def collectionsview(request,name):
     if(Category.objects.filter(name=name,status=0)):
         products=Product.objects.filter(category__name = name)
-        return render(request,"shop/products/index.html",{"products": products})
+        return render(request,"shop/products/index.html",{"products": products,"category_name": name})
     else:
         messages.warning(request,"No Such products found")
         return redirect('collections')
     
+def product_details(request,cname,pname):
+    if(Category.objects.filter(name=cname,status=0)):
+        if(Product.objects.filter(name=pname,status=0)):
+            products=Product.objects.filter(name=pname,status=0).first()
+            return render(request,"shop/products/product_details.html",{"products":products})
+        else:
+            messages.error(request,"No Such products found")
+            return redirect('collections')
+    else:
+        messages.error(request,"No Such products found")
+        return redirect('collections')
+    # return redirect('collections')
